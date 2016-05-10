3a49d79c1b18675d1af47b9378c7d153fade0e40
ashwingadam1 <ashwin.yathracity@gmail.com>
2016-05-11 00:03:45 +0530

    commit

--- src/voting/VoetingReducer.java
+++ src/voting/VoetingReducer.java
@@ -1,27 +1,30 @@
 package voting;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import java.util.regex.*;
 
 public class VoetingReducer extends Reducer<Text, Text, Text, LongWritable> {
 	@Override
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-		long value=0;
+		long value = 0;
 		for (Text val : values) {
-			String str=val.toString();
-			if(Pattern.matches("[0-9]+", str))
-			{
-				value=Integer.parseInt(str);
+			String str = val.toString();
+			if (Pattern.matches("[0-9]+", str)) {
+				value = Integer.parseInt(str);
+				break;
 			}
-			if(Pattern.matches("[a-zA-Z]", str))
-			{
+		}
+		for (Text val : values) {
+			String str = val.toString();
+			if (Pattern.matches("[a-zA-Z]", str)) {
 				context.write(new Text(val), new LongWritable(value));
 			}
 		}
+
 	}
 
 }
